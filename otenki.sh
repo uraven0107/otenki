e02e20d65ec2f6cc14c36adbaef383b22bb204fb
uraven0107 <turn.pso@gmail.com>
2021-09-07 23:49:11 +0900

    :bath: clear functions

--- otenki.sh
+++ otenki.sh
@@ -14,35 +14,28 @@ main=''
 temp_min=0
 temp_max=0
 
-function fetch_data() {
-				#data=$(curl -X GET --connect-timeout 30 -s "https://api.openweathermap.org/data/2.5/weather?id=${ID}&units=metric&lang=ja&appid=${APP_KEY}")
-				data=$(cat './test_data/res_sample.json')
-}
+#data=$(curl -X GET --connect-timeout 30 -s "https://api.openweathermap.org/data/2.5/weather?id=${ID}&units=metric&lang=ja&appid=${APP_KEY}")
+data=$(cat './test_data/res_sample.json')
 
-function parse_data() {
-				# HTTP STATUS_CODE
-				if [[ $data =~ (\"cod\":[0-9]{3}) ]]; then
-								status_cd=$(echo ${BASH_REMATCH[1]} | awk -F '[:]' '{print $2}')
-				fi
+# HTTP STATUS_CODE
+if [[ $data =~ (\"cod\":[0-9]{3}) ]]; then
+				status_cd=$(echo ${BASH_REMATCH[1]} | awk -F '[:]' '{print $2}')
+fi
 
-				# お天気
-				if [[ $data =~ (\"main\":\"[a-zA-Z]+\") ]]; then
-								main=${BASH_REMATCH[1]}
-				fi
+# お天気
+if [[ $data =~ (\"main\":\"[a-zA-Z]+\") ]]; then
+				main=${BASH_REMATCH[1]}
+fi
 
-				# 最低気温
-				if [[ $data =~ (\"temp_min\":[0-9]{1,3}\.[0-9]{1,3}) ]]; then
-								temp_min=${BASH_REMATCH[1]}
-				fi
+# 最低気温
+if [[ $data =~ (\"temp_min\":[0-9]{1,3}\.[0-9]{1,3}) ]]; then
+				temp_min=${BASH_REMATCH[1]}
+fi
 
-				# 最高気温
-				if [[ $data =~ (\"temp_max\":[0-9]{1,3}\.[0-9]{1,3}) ]]; then
-								temp_max=${BASH_REMATCH[1]}
-				fi
-}			
-
-fetch_data
-parse_data
+# 最高気温
+if [[ $data =~ (\"temp_max\":[0-9]{1,3}\.[0-9]{1,3}) ]]; then
+				temp_max=${BASH_REMATCH[1]}
+fi
 
 if [[ $status_cd -ge 400 ]]; then
 				echo "APIの実行で問題が発生しました。 HTTP_STATUS_CD=$status_cd"

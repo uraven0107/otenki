b07b39d96e401f4cb34eb272eb4553c16e9439bf
uraven0107 <turn.pso@gmail.com>
2021-09-08 22:59:01 +0900

    :wrench: fix

--- otenki.sh
+++ otenki.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 CONFIG_FILE='./.cfg'
 if [[ ! -f $CONFIG_FILE ]]; then
@@ -6,20 +6,37 @@ if [[ ! -f $CONFIG_FILE ]]; then
 	exit 1
 fi
 source ./.cfg
-ID=1859393 # 木更津
 
 data=''
 status_cd=0
+name=''
 main=''
-temp_min=0
-temp_max=0
+temp_min=''
+temp_max=''
 
-#data=$(curl -X GET --connect-timeout 30 -s "https://api.openweathermap.org/data/2.5/weather?id=${ID}&units=metric&lang=ja&appid=${APP_KEY}")
-data=$(cat './test_data/res_sample.json')
+# v1.0 日本語未対応
+data=$(curl -X GET --max-time 30 -s "https://api.openweathermap.org/data/2.5/weather?id=${ID}&units=metric&lang=en&appid=${APP_KEY}")
+#data=$(cat './test_data/res_sample.json')
+
+if [[ $? -ne 0 ]]; then
+	echo "APIの実行で問題が発生しました。 \$?=$?"
+	exit 1
+fi
 
 # HTTP STATUS_CODE
 if [[ $data =~ (\"cod\":[0-9]{3}) ]]; then
 	status_cd=$(echo ${BASH_REMATCH[1]} | awk -F '[:]' '{print $2}')
+fi
+
+if [[ $status_cd -ge 400 ]]; then
+	echo "APIの実行で問題が発生しました。 HTTP_STATUS_CD=$status_cd"
+	exit 1
+fi
+
+# 都市名
+# v1.0 日本語未対応
+if [[ $data =~ (\"name\":\"[a-zA-Z]+\") ]]; then
+	name=${BASH_REMATCH[1]}
 fi
 
 # お天気
@@ -37,11 +54,7 @@ if [[ $data =~ (\"temp_max\":[0-9]{1,3}\.[0-9]{1,3}) ]]; then
 	temp_max=${BASH_REMATCH[1]}
 fi
 
-if [[ $status_cd -ge 400 ]]; then
-	echo "APIの実行で問題が発生しました。 HTTP_STATUS_CD=$status_cd"
-	exit 1
-fi
-
+echo $name
 echo $main
 echo $temp_min
 echo $temp_max
